f846c5749d5c81ee47cdcdfffc91ead78fdccc34
manjujeewa <rasika@emakelabs.com>
2019-02-24 08:50:46 +0530

    Updates

---
+++ precache-manifest.8315a74da7d9d03379cbece0b7569ca1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60c08ab0128f7b56f89d",
+    "url": "/XReact/static/css/main.b89abd53.chunk.css"
+  },
+  {
+    "revision": "60c08ab0128f7b56f89d",
+    "url": "/XReact/static/js/main.60c08ab0.chunk.js"
+  },
+  {
+    "revision": "06c772a62ab137412bbf",
+    "url": "/XReact/static/js/runtime~main.06c772a6.js"
+  },
+  {
+    "revision": "2d083f6c674dd00fce26",
+    "url": "/XReact/static/js/2.2d083f6c.chunk.js"
+  },
+  {
+    "revision": "f598e64e8ef84d381992f2a049ee87b7",
+    "url": "/XReact/index.html"
+  }
+];
